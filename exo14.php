3e3cc63622ae36826ef1fb93caca3b771eb1d36f
x0-3 <steph6363@gmail.com>
2022-12-20 11:51:55 +0100

    class voitureElec added

--- exo14.php
+++ exo14.php
@@ -1,13 +1,14 @@
 <?php
 
 class voiture{
-    private $marque;
-    private $modèle;
+    public $marque;
+    public $modèle;
 
-    public function __construct($marque, $modèle)
+    public function __construct($marque, $modèle,$autonomie)
     {
         $this ->marque=$marque;
         $this ->modèle=$modèle;
+        $this ->autonomie=$autonomie;
     }
 
     public function get_marque(){
@@ -15,34 +16,29 @@ class voiture{
     }
 
     public function get_modèle(){
-        return $this->marque;
+        return $this->modèle;
     }
 
     public function getInfo(){
-        echo "Le modele et la marque est : ". $this->marque ." ". $this->modèle;
+        echo "Le modele et la marque est : ". $this->marque ." ". $this->modèle . "<br>";
+        echo "a une autonomie de : " . $this->autonomie;
+
     }
 }
 
 class VoitureElec extends voiture{
     public $autonomie;
 
-    public function set_autonomie($autonomie)
-    {
-        $this->autonomie=$autonomie;
-    }
-
     public function get_autonomie(){
-        echo "a une autonomie de : " . $this->autonomie;
         return $this->autonomie;
     }
 }
 
 
-$v1= new voiture ("Peugeot","408");
+$v1= new voiture ("Peugeot","408","");
 $ve1= new VoitureElec ("BMW","I3",100);
 
 echo $v1->getInfo()."<br>";
 echo $ve1->getInfo()."<br>";
-echo $ve1->get_autonomie()."<br>";
 
 ?>
